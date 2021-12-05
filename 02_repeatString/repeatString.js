f831fcac5bd4fc170af642441ab6c60e21da7203
MSchwartz55 <mps520@nyu.edu>
2021-12-05 15:03:08 -0500

    added logic to pass first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function (text, num) {
+    let result = '';
 
+    for (let i = 0; i < num; i++) {
+        result = result.concat(text);
+    }
+
+    return result;
 };
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
