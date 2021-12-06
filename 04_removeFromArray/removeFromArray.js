ba8348449df3376badec9eeb2860be24f0b509b5
MSchwartz55 <mps520@nyu.edu>
2021-12-06 12:14:18 -0500

    completed exercises 4-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+    for (let i = 1; i < arguments.length; i++) {
+        let index = 0;
+        while (index < arr.length) {
+            if (arguments[i] === arr[index]) {
+                arr.splice(index, 1);
+                i--;
+            }
+            index++;
+        }
+    }
+    return arr;
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4));
 // Do not edit below this line
 module.exports = removeFromArray;
